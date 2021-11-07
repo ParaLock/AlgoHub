7b4be2716d0375a15ed228f2dbbff0031c32bda1
chernikodv <dimon.chr98@gmail.com>
2021-11-07 14:11:51 -0500

    Refactored data source and added new code related responses and error messages

---
+++ backend/src/main/java/edu/wpi/cs/dss/serverless/util/ErrorMessage.java
@@ -0,0 +1,16 @@
+package edu.wpi.cs.dss.serverless.util;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PACKAGE)
+public enum ErrorMessage {
+    SQL_EXECUTION_EXCEPTION("Could not execute SQL statement ..."),
+    RESOURCE_NOT_FOUND_EXCEPTION("Could not find a resource by the given id ..."),
+    AWS_S3_UPLOAD_EXCEPTION("Could not upload an implementation to S3 bucket ..."),
+    AWS_S3_DELETE_EXCEPTION("Could not delete an uploaded implementation from S3 bucket ...");
+
+    private final String value;
+}

1c183ed95059635539f3672b2ba5635a3fa2bb02
wszhan <19308564+wszhan@users.noreply.github.com>
2021-11-08 20:34:15 -0500

    add classification (#5)
    * can manually add classification
    
    * lambda worked locally and remotely; also worked inside api gateway
    
    * adding classification done
    
    * update db.table name
    
    * Refactored add classification related code.
    
    * Added status code to response.
    
    * Added id to ImplementationAddResponse.java
    
    * Added dot before extension
    
    * Added authorId to add classification request/response. Updated SQL DDL script.
    
    Co-authored-by: Weisi Zhan <wszhan@Weisis-MacBook-Pro-3.local>
    Co-authored-by: chernikodv <dimon.chr98@gmail.com>

--- backend/src/main/java/edu/wpi/cs/dss/serverless/classifications/ClassificationAddHandler.java
+++ backend/src/main/java/edu/wpi/cs/dss/serverless/classifications/ClassificationAddHandler.java
@@ -1,28 +1,73 @@
 package edu.wpi.cs.dss.serverless.classifications;
 
-import com.amazonaws.Request;
-import com.amazonaws.services.lambda.runtime.CognitoIdentity;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.s3.AmazonS3;
 import edu.wpi.cs.dss.serverless.classifications.http.ClassificationAddRequest;
 import edu.wpi.cs.dss.serverless.classifications.http.ClassificationAddResponse;
-import edu.wpi.cs.dss.serverless.classifications.model.HierarchyEntry;
+import edu.wpi.cs.dss.serverless.generic.GenericResponse;
+import edu.wpi.cs.dss.serverless.util.DataSource;
+import edu.wpi.cs.dss.serverless.util.ErrorMessage;
+import edu.wpi.cs.dss.serverless.util.HttpStatus;
 
-import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.UUID;
 
-public class ClassificationAddHandler implements RequestHandler<ClassificationAddRequest, ClassificationAddResponse> {
+public class ClassificationAddHandler implements RequestHandler<ClassificationAddRequest, GenericResponse> {
 
-    private AmazonS3 s3 = null;
-
-    LambdaLogger logger;
+    private LambdaLogger logger;
 
     @Override
-    public ClassificationAddResponse handleRequest(ClassificationAddRequest req, Context context) {
+    public GenericResponse handleRequest(ClassificationAddRequest request, Context context) {
         logger = context.getLogger();
-        logger.log("test" + req.classificationInfo.name);
+        logger.log("Received an add classification request from AWS Lambda:\n" + request);
 
-        return new ClassificationAddResponse("test123", "no error", "200");
+        // save algorithm to the database
+        final GenericResponse response = save(request);
+        logger.log("Sent an add classification response to AWS Lambda:\n" + response);
+
+        return response;
+    }
+
+    private GenericResponse save(ClassificationAddRequest request) {
+        final String name = request.getName();
+        final String parentId = request.getParentId();
+        final String authorId = request.getAuthorId();
+        final String id = UUID.randomUUID().toString();
+
+        // creating a sql query
+        final String query = "INSERT INTO classification (id, name, parent_id, author_id) VALUES (?, ?, ?, ?)";
+
+        try (final Connection connection = DataSource.getConnection(logger);
+             final PreparedStatement preparedStatement = connection.prepareStatement(query)
+        ) {
+            logger.log("Successfully connected to db!");
+
+            preparedStatement.setString(1, id);
+            preparedStatement.setString(2, name);
+            preparedStatement.setString(3, parentId);
+            preparedStatement.setString(4, authorId);
+
+            final int rowsAffected = preparedStatement.executeUpdate();
+            logger.log("Insert classification statement has affected " + rowsAffected + " rows!");
+
+            return ClassificationAddResponse.builder()
+                    .statusCode(HttpStatus.SUCCESS.getValue())
+                    .id(id)
+                    .name(name)
+                    .parentId(parentId)
+                    .authorId(authorId)
+                    .build();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            logger.log(ErrorMessage.SQL_EXECUTION_EXCEPTION.getValue());
+            return GenericResponse.builder()
+                    .statusCode(HttpStatus.BAD_REQUEST.getValue())
+                    .error(ErrorMessage.SQL_EXECUTION_EXCEPTION.getValue())
+                    .build();
+        }
     }
 }
--- backend/src/main/java/edu/wpi/cs/dss/serverless/classifications/http/ClassificationRequest.java
+++
@@ -1,6 +0,0 @@
-package edu.wpi.cs.dss.serverless.classifications.http;
-
-public class ClassificationRequest {
-
-    public String id;
-}

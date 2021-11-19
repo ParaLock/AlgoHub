c9926affe056033d2d45fd7c2a92ba4c78029344
swoosh1337 <igrigolia@wpi.edu>
2021-11-19 17:05:07 -0500

    Started working on RemoveAlgorithm and RemoveClassificaiton

--- backend/src/main/java/edu/wpi/cs/dss/serverless/algorithms/http/AlgorithmRemoveResponse.java
+++ backend/src/main/java/edu/wpi/cs/dss/serverless/algorithms/http/AlgorithmRemoveResponse.java
@@ -1,18 +1,11 @@
 package edu.wpi.cs.dss.serverless.algorithms.http;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
+
 import edu.wpi.cs.dss.serverless.generic.GenericResponse;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.SneakyThrows;
 import lombok.experimental.SuperBuilder;
 
 
 @SuperBuilder
 public class AlgorithmRemoveResponse  extends GenericResponse {
-
-
 
 }

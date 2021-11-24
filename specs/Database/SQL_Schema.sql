27dceb1513058ea8b6ff5b2edb041d153cecae54
chernikodv <dimon.chr98@gmail.com>
2021-11-24 14:41:07 -0500

    Updated algohub SQL schema.

--- specs/Database/SQL_Schema.sql
+++ specs/Database/SQL_Schema.sql
@@ -1,11 +1,14 @@
+USE algohub;
+
 CREATE TABLE classification (
    id VARCHAR(36) NOT NULL,
    name VARCHAR(100) NOT NULL,
-   parent_id VARCHAR(36)
+   parent_id VARCHAR(36) NULL,
+   author_id VARCHAR(36) NOT NULL
 );
 
 ALTER TABLE classification ADD CONSTRAINT classification_pk PRIMARY KEY (id);
-ALTER TABLE classification ADD CONSTRAINT classification_parent_id_fk FOREIGN KEY (parent_id) REFERENCES classification (id);
+ALTER TABLE classification ADD CONSTRAINT classification_parent_id_fk FOREIGN KEY (parent_id) REFERENCES classification (id) ON DELETE CASCADE;
 
 CREATE TABLE algorithm (
    id VARCHAR(36) NOT NULL,
@@ -16,20 +19,18 @@ CREATE TABLE algorithm (
 );
 
 ALTER TABLE algorithm ADD CONSTRAINT algorithm_pk PRIMARY KEY (id);
-ALTER TABLE algorithm ADD CONSTRAINT algorithm_classification_id_fk FOREIGN KEY (classification_id) REFERENCES classification (id);
+ALTER TABLE algorithm ADD CONSTRAINT algorithm_classification_id_fk FOREIGN KEY (classification_id) REFERENCES classification (id) ON DELETE CASCADE;
 
 CREATE TABLE implementation (
    id VARCHAR(36) NOT NULL,
    name VARCHAR(100) NOT NULL,
    filename VARCHAR(255) NOT NULL,
    algorithm_id VARCHAR(36) NOT NULL,
-   author_id VARCHAR(36) NOT NULL,
-   PRIMARY KEY (id),
-   FOREIGN KEY (algorithm_id) REFERENCES algorithm (id)
+   author_id VARCHAR(36) NOT NULL
 );
 
 ALTER TABLE implementation ADD CONSTRAINT implementation_pk PRIMARY KEY (id);
-ALTER TABLE implementation ADD CONSTRAINT implementation_algorithm_id_fk FOREIGN KEY (algorithm_id) REFERENCES algorithm (id);
+ALTER TABLE implementation ADD CONSTRAINT implementation_algorithm_id_fk FOREIGN KEY (algorithm_id) REFERENCES algorithm (id) ON DELETE CASCADE;
 
 CREATE TABLE problem_instance (
    id VARCHAR(36) NOT NULL,
@@ -41,7 +42,7 @@ CREATE TABLE problem_instance (
 );
 
 ALTER TABLE problem_instance ADD CONSTRAINT problem_instance_pk PRIMARY KEY (id);
-ALTER TABLE problem_instance ADD CONSTRAINT problem_instance_algorithm_id_fk FOREIGN KEY (algorithm_id) REFERENCES algorithm (id);
+ALTER TABLE problem_instance ADD CONSTRAINT problem_instance_algorithm_id_fk FOREIGN KEY (algorithm_id) REFERENCES algorithm (id) ON DELETE CASCADE;
 
 CREATE TABLE benchmark (
    id VARCHAR(36) NOT NULL,
@@ -60,8 +61,5 @@ CREATE TABLE benchmark (
 );
 
 ALTER TABLE benchmark ADD CONSTRAINT benchmark_pk PRIMARY KEY (id);
-ALTER TABLE benchmark ADD CONSTRAINT benchmark_implementation_id_fk FOREIGN KEY (implementation_id) REFERENCES implementation (id);
-ALTER TABLE benchmark ADD CONSTRAINT benchmark_problem_instance_id_fk FOREIGN KEY (problem_instance_id) REFERENCES problem_instance (id);
-
--- add author_id column to classification table
-ALTER TABLE classification ADD COLUMN author_id VARCHAR(36) NOT NULL;
+ALTER TABLE benchmark ADD CONSTRAINT benchmark_implementation_id_fk FOREIGN KEY (implementation_id) REFERENCES implementation (id) ON DELETE CASCADE;
+ALTER TABLE benchmark ADD CONSTRAINT benchmark_problem_instance_id_fk FOREIGN KEY (problem_instance_id) REFERENCES problem_instance (id) ON DELETE CASCADE;

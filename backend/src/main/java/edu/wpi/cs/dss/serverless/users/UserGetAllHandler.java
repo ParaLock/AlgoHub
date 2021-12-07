d4ea389c99c584790227e5eda4140e5be0d706b6
Paralock <nathanael.mercaldo@gmail.com>
2021-12-07 01:43:22 -0600

    Modified get users to NOT call cognito service directly as doing so requires me to pay a bunch of money on a nat gateway

--- backend/src/main/java/edu/wpi/cs/dss/serverless/users/UserGetAllHandler.java
+++ backend/src/main/java/edu/wpi/cs/dss/serverless/users/UserGetAllHandler.java
@@ -1,69 +1,79 @@
 package edu.wpi.cs.dss.serverless.users;
 
-import com.amazonaws.services.cognitoidp.model.*;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import edu.wpi.cs.dss.serverless.generic.GenericResponse;
+import edu.wpi.cs.dss.serverless.problemInstances.http.ProblemInstanceGetRequest;
+import edu.wpi.cs.dss.serverless.problemInstances.http.ProblemInstanceGetResponse;
 import edu.wpi.cs.dss.serverless.users.http.UserGetAllRequest;
 import edu.wpi.cs.dss.serverless.users.http.UserGetAllResponse;
-import edu.wpi.cs.dss.serverless.users.http.UserRemoveRequest;
-import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider;
-import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProviderClientBuilder;
-
-
+import edu.wpi.cs.dss.serverless.util.DataSource;
 import edu.wpi.cs.dss.serverless.util.ErrorMessage;
 import edu.wpi.cs.dss.serverless.util.HttpStatus;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class UserGetAllHandler implements RequestHandler<UserGetAllRequest, UserGetAllResponse> {
+public class UserGetAllHandler implements RequestHandler<UserGetAllRequest, GenericResponse> {
 
     private LambdaLogger logger;
 
     @Override
-    public UserGetAllResponse handleRequest(UserGetAllRequest request, Context context) {
+    public GenericResponse handleRequest(UserGetAllRequest request, Context context) {
         logger = context.getLogger();
-        logger.log("Received an list users request from AWS Lambda:\n" + request);
+        logger.log("Received a get users request from AWS Lambda: \n" + request);
 
-        // save problem instance to the database
-        final UserGetAllResponse response = listUsers(request);
-        logger.log("Sent list users response to AWS Lambda:\n" + response);
+        // find problem instance by id
+        final GenericResponse response = getAllUsers();
+        logger.log("Sent a get users response to AWS Lambda:\n" + response);
 
         return response;
     }
 
-    private UserGetAllResponse listUsers(UserGetAllRequest request) {
+    private GenericResponse getAllUsers() {
 
-        AWSCognitoIdentityProvider provider = AWSCognitoIdentityProviderClientBuilder.defaultClient();
+        //create a sql query
+        final String query =
+                "SELECT author_id FROM classification" +
+                        " UNION " +
+                        "SELECT author_id FROM algorithm" +
+                        " UNION " +
+                        "SELECT author_id FROM implementation" +
+                        " UNION " +
+                        "SELECT author_id FROM benchmark";
 
-        try {
-
-            String poolId = System.getenv("USER_POOL_ID");
-            ListUsersRequest listRequest = new ListUsersRequest();
-            listRequest.setUserPoolId(poolId);
+        try (final Connection connection = DataSource.getConnection(logger);
+             final PreparedStatement preparedStatement = connection.prepareStatement(query)
+        ) {
+            logger.log("Successfully connected to db!");
 
             ArrayList<String> users = new ArrayList<String>();
 
-            ListUsersResult response = provider.listUsers(listRequest);
-            response.getUsers().forEach(user -> {
-                        users.add(user.getUsername());
-                    }
-            );
+            try (final ResultSet resultSet = preparedStatement.executeQuery()) {
+
+                while(resultSet.next()) {
+
+                    final String authorId = resultSet.getString(1);
+                    users.add(authorId);
+                }
+            }
 
             return UserGetAllResponse.builder()
                     .statusCode(HttpStatus.SUCCESS.getValue())
-                    .error("")
                     .users(users)
                     .build();
 
-        } catch (Exception e){
+        } catch (SQLException e) {
             e.printStackTrace();
-
+            logger.log(ErrorMessage.SQL_EXECUTION_EXCEPTION.getValue());
+            return GenericResponse.builder()
+                    .statusCode(HttpStatus.BAD_REQUEST.getValue())
+                    .error(ErrorMessage.SQL_EXECUTION_EXCEPTION.getValue())
+                    .build();
         }
-        return UserGetAllResponse.builder()
-                .statusCode(HttpStatus.BAD_REQUEST.getValue())
-                .error("Failed to retrieve user.")
-                .build();
     }
 }

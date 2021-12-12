0e550d6dc8381408f35a7d671e9d5996619da198
ParaLock <nathanael.mercaldo@gmail.com>
2021-12-12 17:11:31 -0500

    UI Branch Integration (#10)
    * Frontend: Fixed implementation loading animation
    
    * Frontend: Implemented auto expanding of hierarchy when new items added
    
    * Frontend: Fixed bug related to loading empty classification hierarchy
    
    * Frontend: Fixed ontology loading bug and added better input validation
    
    * Frontend: Improved implementation file parsing
    
    * Frontend: Began implementation of tree view
    
    * Frontend: Finished treeview
    
    * Frontend: Added node remove to treeview for registered users
    
    * Frontend: Began major refactor
    
    * Frontend: Continued refactor
    
    * Frontend: Continued refactor
    
    * Frontend: Nearly finished big refactor
    
    * Frontend: Finalized main refactor
    
    * Completed add implementation form
    
    * Frontend: Finished implementing forms and added selected item id to url
    
    * Frontend: Finished remove item logic and account management page.
    
    * Frontend: Finished wiring forms to corrisponding api endpoints
    
    * Frontend: Wired up user listing
    
    * Finished wiring up algorithm remove
    
    * Fixed bug
    
    * Finished implementing account management page
    
    * Finished account management page and wired up merge classifications feature
    
    * Fixed typo
    
    * Began final iteration of benchmark listing
    
    * Continued work on benchmark listing
    
    * Finished implementing problem instance listing
    
    * Improved benchmark form
    
    * Fixed typo
    
    * Finished account management page
    
    * Adjusted UI theme to be more 'WPI like'
    
    * Finished implementation of new UI theme
    
    * Finished styling
    
    * Refactored all get/post calls to be executed through request service
    
    * Improved form labeling
    
    * Fixed implementation loading bug
    
    * Fixed minor form bug
    
    * Fixed form bug
    
    * Fixed some UI bugs
    
    * Fixed minor scroll bug
    
    * Fixed minor UI bug and improved formating
    
    * Fixed small bug

--- frontend/src/boundary/panels/ProblemInstancePanel.js
+++ frontend/src/boundary/panels/ProblemInstancePanel.js
@@ -1,5 +1,5 @@
 
-import * as React from 'react';
+import React, { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -8,11 +8,15 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import styled from 'styled-components';
-
+import { Config } from "../common/Config"
+import { CircularProgress } from '@mui/material';
+import {updateCachedSet, updateRemoveRequest} from '../../model/ViewModel'
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-
+import { useSelector, useDispatch } from 'react-redux'
 import { Resizable } from "re-resizable";
+import axios from 'axios';
+import fileDownload from 'js-file-download'
 
 const Wrapper = styled.div`
 
@@ -21,12 +25,15 @@ const Wrapper = styled.div`
     margin: 5px;
     padding: 5px;
     font-size: 10pt;
-    height: 30%;
+    margin-bottom: 0px;
+    height: 100%;
     flex-direction: column;
     overflow: scroll;
     display:flex;
     flex-grow: 1;
     overflow-y: auto;
+
+    margin-top: 15px;
 `;
 
 const Title = styled.h4`
@@ -58,22 +65,111 @@ const ButtonWrapper = styled.div`
 
 
 export default function ProblemInstancePanel(props) {
+
+    var selectedItem = useSelector(state => state.viewModel.selectedOntologyItem);
+    var currentUser = useSelector(state => state.model.currentUser);
+    const dispatch = useDispatch();
+    const problemInstances = useSelector(state => state.viewModel.cachedSets["problem_instance"]);
+
+    const [loadingProblemInstances, setLoadingProblemInstances] = useState(false);
+
+    var handleDownload = (url, filename) => {
+
+        const guidRegex = /.*(\{){0,1}[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}(\}){0,1}_/;
+        filename = filename.replace(guidRegex, "");
+
+        axios.get(url, {
+            responseType: 'blob',
+        })
+        .then((res) => {
+            fileDownload(res.data, filename)
+        })
+    }
+
+    var updateProblemInstances = () => {
+        
+        dispatch(updateCachedSet({
+            name: "problem_instance",
+            state: []
+        }))
+        setLoadingProblemInstances(true)
+
+        props.requestService.executePostRequest(
+            (err, data) => {
+
+                setLoadingProblemInstances(false)
+
+                if (err.length == 0) {
+
+                    dispatch(updateCachedSet({
+                        name: "problem_instance",
+                        state: data.problemInstances
+                    }))
+                }
+
+            },
+            {
+                id: selectedItem.id
+            },
+            "problemInstances/by_algorithm",
+            "",
+            "",
+            false
+        );
+    }
+
+    var deleteProblemInstance = (problemInstance) => {
+
+        dispatch(updateRemoveRequest(
+            {
+                msg: "Are you sure you want to remove problem instance?",
+                item: {
+                    id: problemInstance.id,
+                    typeName: "problem_instance"
+                },
+                initiator: "problem_instance_panel"
+            }
+        ))
+    }
+
+    if(!problemInstances) {
+
+        if(selectedItem) {
+
+            updateProblemInstances();
+        }
+    }
+
+    React.useEffect(() => {
+
+        if(selectedItem && selectedItem.typeName == "algorithm") {
+            updateProblemInstances();
+
+        } else {
+            dispatch(updateCachedSet({
+                name: "problem_instance",
+                state: []
+            }))
+        }
+    
+    }, [selectedItem]);
+
     return (
 
         <Wrapper>
             <Typography variant="h6" align="center" component="div" gutterBottom>
-            Problem Instances
+                Problem Instances
             </Typography>
-            {props.currentUser && 
+            {currentUser &&
                 <ButtonWrapper>
-                    <IconButton color="inherit" size="large" onClick={() => props.onProblemInstanceAdd()}>
+                    <IconButton color="inherit" size="large" onClick={() => props.togglePanel("problem_instance_add_form")}>
                         <AddCircleOutlineIcon />
                     </IconButton>
                 </ButtonWrapper>
             }
-
+            {loadingProblemInstances && <CircularProgress />}
             {
-                props.selectedProblemInstances.map((item) => {
+               problemInstances && problemInstances.map((item) => {
 
 
                     return <ProblemInstanceWrapper>
@@ -81,26 +177,36 @@ export default function ProblemInstancePanel(props) {
                         <table>
 
                             <tr>
-                                <td><i>Input Size:</i></td>
-                                <td>{item.inputSize}</td>
+                                <td><i>ProblemType:</i></td>
+                                <td>{item.problemType}</td>
                             </tr>
                             <tr>
-                                <td><i>Name:</i></td>
-                                <td>{item.name}</td>
+                                <td><i>Input Size:</i></td>
+                                <td>{item.datasetSize}</td>
                             </tr>
                             <tr>
                                 <td>
-                                    <Button size="small"  variant="contained">DOWNLOAD</Button>
+                                <Button
+                                    onClick={() => {
+                                        var filename = Config.S3_PATH + "datasets/" + item.datasetFilename;
+
+                                        if (filename) {
+                                            handleDownload(filename, item.datasetFilename)
+                                        }
+                                    }}
+                                    size="small"
+                                    variant="contained"
+                                >DOWNLOAD</Button>
                                 </td>
                             </tr>
                         </table>
                         <br />
 
-                        {props.currentUser && 
-                        
+                        {currentUser &&
+
                             <ButtonWrapper>
                                 <IconButton color="inherit" size="small">
-                                    <HighlightOffIcon />
+                                    <HighlightOffIcon onClick={() => deleteProblemInstance(item)}/>
                                 </IconButton>
                             </ButtonWrapper>
                         }
@@ -108,7 +214,7 @@ export default function ProblemInstancePanel(props) {
                 })
             }
 
-            {props.selectedProblemInstances.length == 0 && <MsgWrapper>None</MsgWrapper>}
+            {problemInstances && problemInstances.length == 0 && !loadingProblemInstances && <MsgWrapper>None</MsgWrapper>}
 
         </Wrapper>
     );

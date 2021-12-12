7c5749e8a21143378cbdcfc4ca78e1b7402127ae
wszhan <19308564+wszhan@users.noreply.github.com>
2021-12-12 17:12:06 -0500

    Problem instances refactored into main (#9)
    * Added lambda to get problem instances by parent algorithm id
    
    * refactor add problem instance by: 1) add file IO to s3 bucket 2) problem instance now links to algorithms rather than implementations 3) add one field for contents
    
    * refactor get problem instance handler to accommodate the changes regarding the fact that problems intances are now linked to algorithms rather than implementations
    
    * Updated functionality to get problem instances by algorithm.
    
    * remove dataset file from S3 Bucket while executing a valid deleting SQL query
    
    * Fixed var naming issue
    
    Co-authored-by: Paralock <nathanael.mercaldo@gmail.com>
    Co-authored-by: Weisi Zhan <wszhan@Weisis-MacBook-Pro-3.local>
    Co-authored-by: chernikodv <dimon.chr98@gmail.com>

--- backend/src/main/java/edu/wpi/cs/dss/serverless/problemInstances/http/ProblemInstanceGetByAlgorithmRequest.java
+++ backend/src/main/java/edu/wpi/cs/dss/serverless/problemInstances/http/ProblemInstanceGetByAlgorithmRequest.java
@@ -2,22 +2,16 @@ package edu.wpi.cs.dss.serverless.problemInstances.http;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.SneakyThrows;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
 @NoArgsConstructor
-public class ProblemInstanceAddRequest {
+@SuperBuilder
+public class ProblemInstanceGetByAlgorithmRequest {
 
-    private String datasetFilename;
-    private String datasetSize;
-    private String problemType;
-    private String algorithmId;
-    private String authorId;
-    private String sourceCodeBase64;
+    private String id;
 
     @Override
     @SneakyThrows
--- backend/src/main/java/edu/wpi/cs/dss/serverless/problemInstances/model/ProblemInstance.java
+++ backend/src/main/java/edu/wpi/cs/dss/serverless/problemInstances/model/ProblemInstance.java
@@ -1,23 +1,23 @@
-package edu.wpi.cs.dss.serverless.problemInstances.http;
+package edu.wpi.cs.dss.serverless.problemInstances.model;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.SneakyThrows;
 
 @Getter
 @Setter
-@NoArgsConstructor
-public class ProblemInstanceAddRequest {
+@AllArgsConstructor
+public class ProblemInstance {
 
+    private String id;
     private String datasetFilename;
-    private String datasetSize;
+    private int datasetSize;
     private String problemType;
     private String algorithmId;
     private String authorId;
-    private String sourceCodeBase64;
 
     @Override
     @SneakyThrows

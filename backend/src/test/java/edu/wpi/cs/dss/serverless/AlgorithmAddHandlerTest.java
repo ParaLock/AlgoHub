280eca50c6753e2f5303f1c29aa893b74e2cf9b8
swoosh1337 <igrigolia@wpi.edu>
2021-12-13 15:47:03 -0500

    Added test cases for each algorithm lambda functions(coverage is 75% so far)

--- backend/src/test/java/edu/wpi/cs/dss/serverless/AlgorithmAddHandlerTest.java
+++ backend/src/test/java/edu/wpi/cs/dss/serverless/AlgorithmAddHandlerTest.java
@@ -4,7 +4,6 @@ import com.google.gson.Gson;
 import edu.wpi.cs.dss.serverless.algorithms.AlgorithmAddHandler;
 import edu.wpi.cs.dss.serverless.algorithms.http.AlgorithmAddRequest;
 import edu.wpi.cs.dss.serverless.algorithms.http.AlgorithmAddResponse;
-import edu.wpi.cs.dss.serverless.algorithms.http.AlgorithmGetRequest;
 import edu.wpi.cs.dss.serverless.generic.GenericResponse;
 import org.junit.Assert;
 import org.junit.Test;
